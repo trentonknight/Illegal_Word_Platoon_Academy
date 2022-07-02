aa66a0fa12a115a2cb5db6ab44bcfabbcd372977
trentonknight <trentonknight@pop-os.localdomain>
2022-07-02 15:43:56 -0400

    new file:   .vscode/launch.json 	modified:   index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function illegalWord(sentence, word) {
   word = word.toLowerCase();
   sentence = sentence.toLowerCase();
-
-  console.log(sentence.includes(word));
-
+  return sentence.includes(word);
 }
 
-// Uploading to Github
-console.log(!illegalWord('Hello there', 'fudge'));
-console.log(!illegalWord('Hello there', 'there'));
+console.log(illegalWord('Hello there', 'fudge'));
+console.log(illegalWord('Hello there', 'there'));
 console.log(illegalWord("But I didn't say fudge", 'fudge'));
 console.log(illegalWord("But I didn't say fudge", 'Fudge'));
